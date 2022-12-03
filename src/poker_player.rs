73d651d6ae80c876e88c5a9e73ade1afed975ef2
lordfoom <timothys@soliditech.com>
2022-12-03 19:21:26 +0200

    Lychrel numbers
    Love to see the machine crunching numbers in the evening.
    
    Stubs for poker player stuff.

---
+++ src/poker_player.rs
@@ -0,0 +1,7 @@
+use std::io::BufReader;
+
+fn read_poker_file(){
+    //read in the poker file's contects from the file system
+    let pf_contents = BufReader::new(std::fs::File::open("poker_054.txt").expect("Cannot find file"));
+
+}
